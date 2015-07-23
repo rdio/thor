23cc6094fbd87c81f8bd0c42683b598843731007
Arthur Chan <arthur.chan@rd.io>
2015-07-23 11:41:24 -0700

    Add filters for: - Adding a border/frame - Positioning/aligning/fitting text - Added primitives for   - length   - specify percentage from width and height.   - hsl/hsla color, because this isn't the 90s anymore. :P - Add `/help`
    Jon Bender and Mike Smith helped a lot during the review of this patch.

--- src/test/scala/com/rdio/thor/ImageFiltersSpec.scala
+++ src/test/scala/com/rdio/thor/ImageFiltersSpec.scala
@@ -1,7 +1,6 @@
 package com.rdio.thor
 
-import java.awt.{Color, Font}
-
+import java.awt.{Color, Font, Graphics2D}
 import com.sksamuel.scrimage.{Image, Pixel}
 
 class ImageFiltersSpec extends BaseSpec {
@@ -53,7 +52,7 @@ class ImageFiltersSpec extends BaseSpec {
     pixel.alpha should be (255)
   }
 
-  "MaskFilter" should "show overlay image with a white mask" in {
+  it should "show overlay image with a white mask" in {
     val img = Image.filled(5, 5, new Color(0, 0, 255, 255))
     val overlay = Image.filled(5, 5, new Color(255, 0, 0, 255))
     var mask = Image.filled(5, 5, Color.WHITE)
@@ -68,7 +67,7 @@ class ImageFiltersSpec extends BaseSpec {
     pixel.alpha should be (255)
   }
 
-  "MaskFilter" should "blend overlay and original image by mask color" in {
+  it should "blend overlay and original image by mask color" in {
     val img = Image.filled(5, 5, new Color(0, 0, 255, 255))
     val overlay = Image.filled(5, 5, new Color(255, 0, 0, 255))
     var mask = Image.filled(5, 5, Color.BLUE)
@@ -83,7 +82,7 @@ class ImageFiltersSpec extends BaseSpec {
     pixel.alpha should be (255)
   }
 
-  "MaskFilter" should "leave the mask layer untouched" in {
+  it should "leave the mask layer untouched" in {
     val img = Image.filled(5, 5, new Color(0, 0, 255, 255))
     val overlay = Image.filled(5, 5, new Color(255, 0, 0, 255))
     var mask = Image.filled(5, 5, new Color(27, 200, 93, 255))
@@ -96,5 +95,258 @@ class ImageFiltersSpec extends BaseSpec {
     pixel.green should be (200)
     pixel.blue should be (93)
     pixel.alpha should be (255)
+  }
+
+  trait FrameFilterFixture {
+    val width  = 300
+    val height = 400
+    val thickness = 3
+    val imageColor = com.sksamuel.scrimage.Color.White
+    val frameColor = com.sksamuel.scrimage.Color.Black
+    val imagePixel = Pixel(imageColor)
+    val framePixel = Pixel(frameColor)
+    val blank = Image.filled(width, height, imageColor)
+  }
+
+  "FrameFilter" should "fill in the frame regions (thickness specified in pixels)" in {
+    new FrameFilterFixture {
+      val framed = blank.filter(FrameFilter(LengthPixels(3), frameColor))
+      framed.pixel(0, 0) should be (framePixel)
+      framed.pixel(width-1, 0) should be (framePixel)
+      framed.pixel(0, height-1) should be (framePixel)
+      framed.pixel(width-1, height-1) should be (framePixel)
+
+      framed.pixel(thickness,   0)           should be (framePixel)
+      framed.pixel(thickness,   thickness-1) should be (framePixel)
+      framed.pixel(0,           thickness)   should be (framePixel)
+      framed.pixel(thickness-1, thickness)   should be (framePixel)
+      framed.pixel(thickness-1, thickness-1) should be (framePixel)
+
+      framed.pixel(width-thickness-1, 0)           should be (framePixel)
+      framed.pixel(width-thickness-1, thickness-1) should be (framePixel)
+      framed.pixel(width-thickness,   thickness)   should be (framePixel)
+      framed.pixel(width-1,           thickness)   should be (framePixel)
+      framed.pixel(width-thickness,   thickness-1) should be (framePixel)
+
+      framed.pixel(0,           height-thickness-1) should be (framePixel)
+      framed.pixel(thickness-1, height-thickness-1) should be (framePixel)
+      framed.pixel(thickness,   height-thickness)   should be (framePixel)
+      framed.pixel(thickness,   height-1)           should be (framePixel)
+      framed.pixel(thickness-1, height-thickness)   should be (framePixel)
+
+      framed.pixel(width-thickness-1, height-thickness)   should be (framePixel)
+      framed.pixel(width-thickness-1, height-1)           should be (framePixel)
+      framed.pixel(width-thickness,   height-thickness-1) should be (framePixel)
+      framed.pixel(width-1,           height-thickness-1) should be (framePixel)
+      framed.pixel(width-thickness,   height-thickness)   should be (framePixel)
+    }
+  }
+
+  it should "leave the interior of the frame intact (thickness specified in pixels)" in {
+    new FrameFilterFixture {
+      val framed = blank.filter(FrameFilter(LengthPixels(3), frameColor))
+      framed.pixel(thickness,         thickness)          should be (imagePixel)
+      framed.pixel(width-thickness-1, thickness)          should be (imagePixel)
+      framed.pixel(thickness,         height-thickness-1) should be (imagePixel)
+      framed.pixel(width-thickness-1, height-thickness-1) should be (imagePixel)
+    }
+  }
+
+  it should "fill in the frame regions (thickness specified in percentage)" in {
+    new FrameFilterFixture {
+      val percentageThickness = 0.05f
+      val framed = blank.filter(FrameFilter(LengthPercentage(percentageThickness), frameColor))
+      val hThickness = (width * percentageThickness).toInt
+      val vThickness = (height * percentageThickness).toInt
+      framed.pixel(0, 0) should be (framePixel)
+      framed.pixel(width-1, 0) should be (framePixel)
+      framed.pixel(0, height-1) should be (framePixel)
+      framed.pixel(width-1, height-1) should be (framePixel)
+
+      framed.pixel(hThickness,   0)           should be (framePixel)
+      framed.pixel(hThickness,   vThickness-1) should be (framePixel)
+      framed.pixel(0,            vThickness)   should be (framePixel)
+      framed.pixel(hThickness-1, vThickness)   should be (framePixel)
+      framed.pixel(hThickness-1, vThickness-1) should be (framePixel)
+
+      framed.pixel(width-hThickness-1, 0)           should be (framePixel)
+      framed.pixel(width-hThickness-1, vThickness-1) should be (framePixel)
+      framed.pixel(width-hThickness,   vThickness)   should be (framePixel)
+      framed.pixel(width-1,            vThickness)   should be (framePixel)
+      framed.pixel(width-hThickness,   vThickness-1) should be (framePixel)
+
+      framed.pixel(0,            height-vThickness-1) should be (framePixel)
+      framed.pixel(hThickness-1, height-vThickness-1) should be (framePixel)
+      framed.pixel(hThickness,   height-vThickness)   should be (framePixel)
+      framed.pixel(hThickness,   height-1)           should be (framePixel)
+      framed.pixel(hThickness-1, height-vThickness)   should be (framePixel)
+
+      framed.pixel(width-hThickness-1, height-vThickness)   should be (framePixel)
+      framed.pixel(width-hThickness-1, height-1)           should be (framePixel)
+      framed.pixel(width-hThickness,   height-vThickness-1) should be (framePixel)
+      framed.pixel(width-1,            height-vThickness-1) should be (framePixel)
+      framed.pixel(width-hThickness,   height-vThickness)   should be (framePixel)
+    }
+  }
+
+  it should "leave the interior of the frame intact (thickness specified in percentage)" in {
+    new FrameFilterFixture {
+      val percentageThickness = 0.05f
+      val framed = blank.filter(FrameFilter(LengthPercentage(percentageThickness), frameColor))
+      val hThickness = (width * percentageThickness).toInt
+      val vThickness = (height * percentageThickness).toInt
+      framed.pixel(hThickness,         vThickness)          should be (imagePixel)
+      framed.pixel(width-hThickness-1, vThickness)          should be (imagePixel)
+      framed.pixel(hThickness,         height-vThickness-1) should be (imagePixel)
+      framed.pixel(width-hThickness-1, height-vThickness-1) should be (imagePixel)
+    }
+  }
+
+  it should "fill in the frame regions (thickness specified in percentage-width)" in {
+    new FrameFilterFixture {
+      val percentageThickness = 0.05f
+      val framed = blank.filter(FrameFilter(LengthPercentWidth(percentageThickness), frameColor))
+      override val thickness = (width * percentageThickness).toInt
+      framed.pixel(0, 0) should be (framePixel)
+      framed.pixel(width-1, 0) should be (framePixel)
+      framed.pixel(0, height-1) should be (framePixel)
+      framed.pixel(width-1, height-1) should be (framePixel)
+
+      framed.pixel(thickness,   0)           should be (framePixel)
+      framed.pixel(thickness,   thickness-1) should be (framePixel)      
+      framed.pixel(0,           thickness)   should be (framePixel)
+      framed.pixel(thickness-1, thickness)   should be (framePixel)
+      framed.pixel(thickness-1, thickness-1) should be (framePixel)      
+
+      framed.pixel(width-thickness-1, 0)           should be (framePixel)
+      framed.pixel(width-thickness-1, thickness-1) should be (framePixel)
+      framed.pixel(width-thickness,   thickness)   should be (framePixel)
+      framed.pixel(width-1,           thickness)   should be (framePixel)
+      framed.pixel(width-thickness,   thickness-1) should be (framePixel)
+
+      framed.pixel(0,           height-thickness-1) should be (framePixel)
+      framed.pixel(thickness-1, height-thickness-1) should be (framePixel)
+      framed.pixel(thickness,   height-thickness)   should be (framePixel)
+      framed.pixel(thickness,   height-1)           should be (framePixel)
+      framed.pixel(thickness-1, height-thickness)   should be (framePixel)
+
+      framed.pixel(width-thickness-1, height-thickness)   should be (framePixel)
+      framed.pixel(width-thickness-1, height-1)           should be (framePixel)
+      framed.pixel(width-thickness,   height-thickness-1) should be (framePixel)
+      framed.pixel(width-1,           height-thickness-1) should be (framePixel)
+      framed.pixel(width-thickness,   height-thickness)   should be (framePixel)
+    }
+  }
+
+  it should "leave the interior of the frame intact (thickness specified in percentage-width)" in {
+    new FrameFilterFixture {
+      val percentageThickness = 0.05f
+      val framed = blank.filter(FrameFilter(LengthPercentWidth(percentageThickness), frameColor))
+      override val thickness = (width * percentageThickness).toInt
+      framed.pixel(thickness,         thickness)          should be (imagePixel)
+      framed.pixel(width-thickness-1, thickness)          should be (imagePixel)
+      framed.pixel(thickness,         height-thickness-1) should be (imagePixel)
+      framed.pixel(width-thickness-1, height-thickness-1) should be (imagePixel)
+    }
+  }
+
+  // Helpers for testing TextFilter
+
+  def imageHasStuffInRegion(img: Image, baselineColor: Color, x: Int, y: Int, w: Int, h: Int): Boolean = {
+    val region = img.subimage(x, y, w, h)
+    val basePixel = Pixel(baselineColor)
+    !(region.pixels.forall(x => x == basePixel))
+  }
+
+  def imageDoesntHaveStuffOutsideOfRegion(img: Image, baselineColor: Color, x: Int, y: Int, w: Int, h: Int): Boolean = {
+    val image = img.copy
+    val g2 = image.awt.getGraphics.asInstanceOf[Graphics2D]
+    g2.setColor(baselineColor)
+    g2.fillRect(x, y, w, h)
+    g2.dispose()
+
+    val basePixel = Pixel(baselineColor)
+    image.pixels.forall(x => x == basePixel)
+  }
+
+  def imageOnlyHasStuffInRegion(img: Image, baselineColor: Color, x: Int, y: Int, w: Int, h: Int): Boolean = {
+    imageHasStuffInRegion(img, baselineColor, x, y, w, h) && imageDoesntHaveStuffOutsideOfRegion(img, baselineColor, x, y, w, h)
+  }
+
+  trait TextFilterFixture {
+    val font = new Font("Helvetica", Font.PLAIN, 12)
+
+    // these are really strict bounds
+    val expectedTextWidth = 40
+    val expectedTextHeight = 12
+
+    val width  = 300
+    val height = 400
+    val imageColor = com.sksamuel.scrimage.Color.White
+    val textColor = com.sksamuel.scrimage.Color.Black
+    val imagePixel = Pixel(imageColor)
+    val blank = Image.filled(width, height, imageColor)
+    val withText = blank.filter(TextFilter("Testing", font, textColor))    
+  }
+
+  "TextFilter" should "render text in the correct place (centered position, center horizontal alignment, center vertical alignment)" in {
+    new TextFilterFixture {
+      val expectedX = width/2 - expectedTextWidth/2
+      val expectedY = height/2 - expectedTextHeight/2
+
+      imageOnlyHasStuffInRegion(withText, imageColor, expectedX, expectedY, expectedTextWidth, expectedTextHeight)
+    }
+  }
+
+  it should "render text in the correct place (relative position at (0.5, 0.25), center horizontal alignment, top vertical alignment)" in {
+    new TextFilterFixture {
+      val posX = (0.5).toFloat
+      val posY = (0.25).toFloat
+      override val withText = blank.filter(TextFilter("Testing", font, textColor, List(CartesianRelative(posX, posY)), CenterAlign(), TopAlign()))
+  
+      val expectedX = (width * posX) - (expectedTextWidth / 2)
+      val expectedY = height * posY
+
+      imageOnlyHasStuffInRegion(withText, imageColor, expectedX.toInt, expectedY.toInt, expectedTextWidth, expectedTextHeight)
+    }
+  }
+
+  it should "render text in the correct place (relative position at (0.5, 0.25), center horizontal alignment, bottom vertical alignment)" in {
+    new TextFilterFixture {
+      val posX = (0.5).toFloat
+      val posY = (0.25).toFloat
+      override val withText = blank.filter(TextFilter("Testing", font, textColor, List(CartesianRelative(posX, posY)), CenterAlign(), BottomAlign()))
+  
+      val expectedX = (width * posX) - (expectedTextWidth / 2)
+      val expectedY = height * posY - expectedTextHeight
+
+      imageOnlyHasStuffInRegion(withText, imageColor, expectedX.toInt, expectedY.toInt, expectedTextWidth, expectedTextHeight)
+    }
+  }
+
+  it should "be able to fit text to a given width (shrink text to fit)" in {
+    new TextFilterFixture {
+      val posX = 10
+      val posY = 10
+      val testString = "abjsel kfsjdf kldsjk flsdjf lkjouh"
+      override val expectedTextWidth = 200
+      override val expectedTextHeight = 36
+      override val withText = blank.filter(TextFilter(testString, font, textColor, List(CartesianAbsolute(posX, posY)), LeftAlign(), TopAlign(), WidthFitted(expectedTextWidth, LengthPixels(expectedTextHeight))))
+
+      imageOnlyHasStuffInRegion(withText, imageColor, posX, posY, expectedTextWidth, expectedTextHeight)
+    }
+  }
+
+  it should "be able to fit text to a given width (don't exceed maximum font size)" in {
+    new TextFilterFixture {
+      val posX = 10
+      val posY = 10
+      val testString = "Oh Hai!"
+      override val expectedTextWidth = 200
+      override val expectedTextHeight = 36
+      override val withText = blank.filter(TextFilter(testString, font, textColor, List(CartesianAbsolute(posX, posY)), LeftAlign(), TopAlign(), WidthFitted(expectedTextWidth, LengthPixels(expectedTextHeight))))
+
+      imageOnlyHasStuffInRegion(withText, imageColor, posX, posY, expectedTextWidth, expectedTextHeight)
+    }
   }
 }
